db9735ae2503db5ae019e3f95ed6e6db57ccccce
Kevin Nielsen <knielsen24@gmail.com>
2022-05-03 18:44:31 -0700

    commit tree 5cc931bc28095652733c4fa15bffff9008d5d46e

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log(event);
+
+    });
   
 }
 
